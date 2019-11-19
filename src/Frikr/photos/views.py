d71f798881fc8e3ad8fd8f1ca9be2fdbc753f1fc
es2812 <es2812@hotmail.com>
2019-11-19 20:46:00 +0100

    Added list with photo names to home page

--- src/Frikr/photos/views.py
+++ src/Frikr/photos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from photos.models import Photo
 
 def home(request):
-    return HttpResponse("Hello world!")
+    photos = Photo.objects.all()
+    html = '<ul>'
+    for photo in photos:
+        html += '<li>' + photo.name + '</li>'
+    html += '</ul>'
+
+    return HttpResponse(html)
