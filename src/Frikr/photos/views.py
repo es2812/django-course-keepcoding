fd02c9072c3275eada98ead9a2d86d05472c7a66
es2812 <es2812@hotmail.com>
2019-11-20 17:14:33 +0100

    Limited and ordered photos

--- src/Frikr/photos/views.py
+++ src/Frikr/photos/views.py
@@ -3,6 +3,8 @@ from django.http.response import HttpResponse
 from photos.models import Photo
 
 def home(request):
-    photos = Photo.objects.all()
-    
-    return render(request, "photos/home.html", context={"photo_list": photos})
+    photos = Photo.objects.all().order_by('-created_at')
+    context = {
+        "photo_list": photos[0:5]
+    }
+    return render(request, "photos/home.html", context)
