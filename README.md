ad27199f517364cb2249634ff71f76ba088d688b
es2812 <es2812@hotmail.com>
2019-11-21 19:47:55 +0100

    Adding owner and visibility attributes to photos

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This tutorial works with Python 2.6 and Django 1.9. Changes have been made in so
   - The function associated with an url has to be called after importing its containing module (`views.py`), instead of being written down as a string.
 - Section 7:
   - To change the way a model's instance is displayed in the admin page, the `__str__` method is used instead of `__unicode__`.
+- Section 10:
+  - Since Django 2.0 the ForeignKey constructor requires an additional attribute, `on_delete`.
+  - With Python 3 django no longer complains when given an UTF-8 character.
--- src/Frikr/photos/models.py
+++ src/Frikr/photos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models import CASCADE
 COPYRIGHT = 'RIG'
 COPYLEFT = 'LEF'
 CREATIVE_COMMONS = 'CC'
@@ -9,14 +10,25 @@ LICENSES = (
     (COPYLEFT, 'Copyleft'),
     (CREATIVE_COMMONS, 'Creative Commons')
 )
+
+PRIVATE = 'PRI'
+PUBLIC = 'PUB'
+
+VISIBILITY = {
+    (PRIVATE, 'Privada'),
+    (PUBLIC, 'Pública')
+}
 class Photo(models.Model):
 
+    owner = models.ForeignKey(User, on_delete=CASCADE)
     name = models.CharField(max_length=150)
     url = models.URLField()
     description = models.TextField(blank=True, null=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
+
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=PUBLIC)
 
     def __str__(self):
         return self.name
