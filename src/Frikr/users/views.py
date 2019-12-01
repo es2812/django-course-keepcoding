1442a99e7991dd4c86a14d03726ed0d60da8c66c
es2812 <es2812@hotmail.com>
2019-12-01 19:07:18 +0100

    Implemented login

--- src/Frikr/users/views.py
+++ src/Frikr/users/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import logout as django_logout, authenticate, login as django_login
 
 def login(request):
-    return render(request, 'users/login.html')
+    error_msg = []
+    if request.method == 'POST':
+        username = request.POST.get('usr')
+        password = request.POST.get('pwd')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            error_msg.append('Nombre de usuario o contraseña incorrectos')
+        else:
+            if user.is_active:
+                django_login(request, user)
+                return redirect('photos_home')
+            else:
+                error_msg.append('Usuario no activo')
+    context = {
+        "errors": error_msg
+    }
+    return render(request, 'users/login.html', context)
 
 def logout(request):
     if request.user.is_authenticated:
