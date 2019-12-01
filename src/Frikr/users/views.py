6c80b44cc425a7f5ab0de79149b9a41b9c2e4000
es2812 <es2812@hotmail.com>
2019-12-01 14:47:56 +0100

    Implemented login view

--- src/Frikr/users/views.py
+++ src/Frikr/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout
 
 def login(request):
-    pass
+    return render(request, 'users/login.html')
 
 def logout(request):
     if request.user.is_authenticated:
