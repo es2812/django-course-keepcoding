ab35b72a448bb87de302a54ca3d6f6984d3503d2
es2812 <es2812@hotmail.com>
2019-12-15 17:24:45 +0100

    Implemented login with Django Forms

--- src/Frikr/users/views.py
+++ src/Frikr/users/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout, authenticate, login as django_login
+from users.forms import LoginForm
 
 def login(request):
     error_msg = []
     if request.method == 'POST':
-        username = request.POST.get('usr')
-        password = request.POST.get('pwd')
+        form = LoginForm(request.POST)
+        username = form.get('usr')
+        password = form.get('pwd')
         user = authenticate(username=username, password=password)
         if user is None:
             error_msg.append('Nombre de usuario o contraseña incorrectos')
@@ -15,7 +17,10 @@ def login(request):
                 return redirect('photos_home')
             else:
                 error_msg.append('Usuario no activo')
+    else:
+        form = LoginForm()
     context = {
+        "login_form": form,
         "errors": error_msg
     }
     return render(request, 'users/login.html', context)
