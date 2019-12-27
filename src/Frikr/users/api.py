86c51a94279513a3295abb70d5b7a688ccc69fb1
es2812 <es2812@hotmail.com>
2019-12-27 12:51:39 +0100

    Using APIView to render user list API

--- src/Frikr/users/api.py
+++ src/Frikr/users/api.py
@@ -1,17 +1,13 @@
-from django.views.generic import View
 from django.contrib.auth.models import User
 from users.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
-from django.http.response import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         serializer_users = serializer.data #list of dicts
 
-        renderer = JSONRenderer()
-        json_users = renderer.render(serializer_users)
-
-        return HttpResponse(json_users)
+        return Response(serializer_users)
