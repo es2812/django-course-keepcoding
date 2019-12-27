514c74b0db8c97bdf47701b5c505cb140c1f4d2a
es2812 <es2812@hotmail.com>
2019-12-27 15:37:36 +0100

    Implemented user deletion API

--- src/Frikr/users/api.py
+++ src/Frikr/users/api.py
@@ -38,3 +38,9 @@ class UserDetailAPI(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id):
+        user = get_object_or_404(User, pk=user_id)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
