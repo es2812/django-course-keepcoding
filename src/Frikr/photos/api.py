22cecb610f0d61e20d7979278e6ec6af521a8486
es2812 <es2812@hotmail.com>
2019-12-27 16:21:43 +0100

    Switched photo list API to use a generic APIView

--- src/Frikr/photos/api.py
+++ src/Frikr/photos/api.py
@@ -2,11 +2,8 @@ from rest_framework.views import APIView
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 
-class PhotoListAPI(APIView):
-
-    def get(self, request):
-        photos = Photo.objects.all()
-        serializer = PhotoSerializer(photos, many=True)
-
-        return Response(serializer.data)
+class PhotoListAPI(ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
