1ba5fde2d2f9cfe68d17b8df665bb5c63ad319df
es2812 <es2812@hotmail.com>
2019-12-27 17:57:22 +0100

    Adapting API photo list to website's photo list (customized to auth user)

--- src/Frikr/photos/api.py
+++ src/Frikr/photos/api.py
@@ -1,18 +1,26 @@
 from rest_framework.views import APIView
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
+from photos.views import PhotosQueryset
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class PhotoListAPI(ListCreateAPIView):
+class PhotoListAPI(PhotosQueryset, ListCreateAPIView):
     queryset = Photo.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
 
-class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
-    queryset = Photo.objects.filter()
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
+
+
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView, PhotosQueryset):
+    queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
