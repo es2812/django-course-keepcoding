beb70e015c1a999e0e9ed5c7978d6111c56cf001
es2812 <es2812@hotmail.com>
2019-12-27 17:47:12 +0100

    Implemented authorization in API

--- src/Frikr/photos/api.py
+++ src/Frikr/photos/api.py
@@ -3,9 +3,11 @@ from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PhotoListAPI(ListCreateAPIView):
     queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
@@ -13,3 +15,4 @@ class PhotoListAPI(ListCreateAPIView):
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.filter()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
