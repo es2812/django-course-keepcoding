4f6d009c11ea43761b20cf607c53e5e3765f2af9
es2812 <es2812@hotmail.com>
2019-12-27 12:59:38 +0100

    Implementing user details on API

--- src/Frikr/users/api.py
+++ src/Frikr/users/api.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from users.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 class UserListAPI(APIView):
 
@@ -11,3 +12,12 @@ class UserListAPI(APIView):
         serializer_users = serializer.data #list of dicts
 
         return Response(serializer_users)
+
+
+class UserDetailAPI(APIView):
+
+    def get(self, request, user_id):
+        user = get_object_or_404(User, pk=user_id)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
